777dc233807486e263dfd0edc22c42c54fd7e82e
flari949 <riley.flanagan15@gmail.com>
2023-08-08 10:54:28 +1200

    Implemented JSON processing library and basic use

--- data_fetch.cpp
+++ data_fetch.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
-
+#include <rapidjson/document.h>
+  
+// Callback function
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
     size_t totalSize = size * nmemb;
     output->append(static_cast<char*>(contents), totalSize);
     return totalSize;
 }
 
+// Function to retrieve API data under given specifications
 std::string fetchDataFromAPI(const std::string& apiUrl, const std::string& username, const std::string& password) {
     CURL* curl = curl_easy_init();
     std::string response;
@@ -41,8 +44,8 @@ std::string fetchDataFromAPI(const std::string& apiUrl, const std::string& usern
 }
 
 int main() {
-    // Replace these with your API endpoint and authentication credentials
-    std::string apiUrl = "https://api.eventfinda.co.nz/v2/artists.json?rows=1&q=henry";
+    std::string apiUrl = "https://api.eventfinda.co.nz/v2/events.json?rows=1&fields=event:(url)";
+    // Need to add as env variables for security
     std::string username = "eventure";
     std::string password = "5jryqspxvxkk";
 
@@ -50,7 +53,36 @@ int main() {
     std::string responseData = fetchDataFromAPI(apiUrl, username, password);
 
     // Print the response data
-    std::cout << "API Response:\n" << responseData << std::endl;
+    // std::cout << "API Response:\n" << responseData << std::endl;
+
+    // Parse the JSON string
+    rapidjson::Document doc;
+    doc.Parse(responseData.c_str());
+
+    // Check if parsing was successful
+    if (!doc.HasParseError()) {
+        // Check for events array
+        if (doc.HasMember("events") && doc["events"].IsArray()) {
+            const rapidjson::Value& events = doc["events"];
+            // Check events array populated
+            if (!events.Empty() && events[0].IsObject()) {
+                const rapidjson::Value& event = events[0];
+                // Check url object present
+                if (event.HasMember("url") && event["url"].IsString()) {
+                    const char* url = event["url"].GetString();
+                    std::cout << "Response url: " << url << std::endl;
+                } else {
+                    std::cerr << "URL not found in JSON" << std::endl;
+                }
+            } else {
+                std::cerr << "Events array is empty or not an object" << std::endl;
+            }
+        } else {
+            std::cerr << "Events array not found in JSON" << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to parse JSON data" << std::endl;
+    }
 
     return 0;
 }
